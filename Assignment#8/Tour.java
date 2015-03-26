0424cc77dae32297d34f0266f0ca5fa6f588545d
zapstart <zapstart@gmail.com>
2015-03-25 21:35:27 -0700

    Assignment#8

---
+++ Assignment#8/Tour.java
@@ -0,0 +1,31 @@
+public class Tour {
+
+    public class Node {
+        private Point p;
+        private Node next;
+    }
+    
+    public Tour() {
+    }
+    
+    public Tour(Point a, Point b, Point c, Point d) {
+    }
+
+    public void show() {
+    }
+
+    public void draw() {
+    }
+
+    public int size() {
+    }
+
+    public double distance() {
+    }
+
+    public void insertNearest(Point p) {
+    }
+
+    public void insertSmallest(Point p) {
+    }
+}
