61ef388f084840b7352fbe64d1210ac8acc52ec0
zapstart <zapstart@gmail.com>
2015-04-10 17:18:32 -0700

    add comments and fix bugs
    change show information

--- Assignment#9/BlobFinder.java
+++ Assignment#9/BlobFinder.java
@@ -2,16 +2,23 @@ import java.awt.Color;
 
 public class BlobFinder {
     private int p_width, p_height;
+    
+    // get all luminance value for each pixel
     private double[][] pixel_value; 
+    
+    // store all foreground pixe 
     private boolean[][] blob; 
+    
+    // store all isolated Blobs    
     private Blob[][] blob_array; 
     private Luminance luminance = new Luminance(); 
     private int bead_num = 0;
-
+    
     public class Blob {
         private int x, y; 
         private double mass;
 
+        // Blob constructor
         public Blob() {
             x = 0;
             y = 0;
@@ -53,12 +60,14 @@ public class BlobFinder {
         }
     }
     
+    // BlobFinder constructor 
     public BlobFinder(Picture picture, double tau) {
         p_width = picture.width(); 
         p_height = picture.height(); 
         pixel_value = new double[p_width][p_height];  
         blob = new boolean[p_width][p_height]; 
         
+        // init pixel_value and blob 
         for (int i = 0; i < p_width; i++) {
             for (int j = 0; j < p_height; j++) {
                 pixel_value[i][j] = luminance.lum(picture.get(i, j));
@@ -67,19 +76,20 @@ public class BlobFinder {
         }
     }
     
-    private void find_blob_dfs() {
+    // use dfs to find all beads
+    public void find_blob_dfs(int P) {
         blob_array = new Blob[p_width][p_height]; 
         for (int i = 0; i < p_width; i++) {
             for (int j = 0; j < p_height; j++) {
                 blob_array[i][j] = new Blob();
                 single_blob_dfs(blob, i, j, blob_array[i][j]);
-                //if (blob_array[i][j].mass() != 0) System.out.println(blob_array[i][j].toString());  
-                if (blob_array[i][j].mass() != 0) bead_num++;
+                if (blob_array[i][j].mass() >= P) bead_num++;
             }
         }
     }
     
-    private void single_blob_dfs(boolean[][] blob, int x, int y, Blob new_blob) {
+    // dfs single step to search beads
+    public void single_blob_dfs(boolean[][] blob, int x, int y, Blob new_blob) {
         if ((x < 0) || (x > p_width - 1)) return; 
         if ((y < 0) || (y > p_height - 1)) return; 
         if (blob[x][y]) return;
@@ -91,28 +101,17 @@ public class BlobFinder {
         single_blob_dfs(blob, x, y - 1, new_blob); 
     }
     
-    private Blob[] getBeads(int P) {
+    // get all beads  
+    public Blob[] getBeads(int P) {
         Blob[] beads = new Blob[bead_num];
         for (int i = 0; i < p_width; i++) {
             for (int j = 0; j < p_height; j++) {
-                if (blob_array[i][j].mass() > P) {
+                if (blob_array[i][j].mass() >= P) {
                     beads[--bead_num] = blob_array[i][j];
-                    System.out.println(beads[bead_num]);
                 }
             }
         } 
         return beads;
     }
 
-    public static void main(String args[]) {
-        Picture picture = new Picture("frame00001.jpg");
-        BlobFinder blobfinder = new BlobFinder(picture, 180);
-        blobfinder.find_blob_dfs();  
-        blobfinder.getBeads(25);  
-    }
-    
-    
-    
-    //public Blob[] getBeads(int P) {
-    //}
 }
